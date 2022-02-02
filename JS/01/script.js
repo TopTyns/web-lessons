177bfb81b40ab9207a3ff708f503bc32d14266f3
TopTyns <dmitriubogutskiu2@gmail.com>
2022-02-02 14:37:43 +0200

    fix

--- JS/01/script.js
+++ JS/01/script.js
@@ -1,27 +1,43 @@
 function convert() {
     let kg = prompt('Введіть кількість кілограм:');
+    let kilogram = Number(kg);
+    console.log(typeof(kilogram));
+    if(kilogram>0 && !isNaN(kilogram) ){
+        let pounds = kg / 2.20;
+        let ounces = kg / 35.2;
+        let centners = kg / 100;
+        let tons = kg / 1000;
 
-    let pounds = kg / 2.20;
-    let ounces = kg / 35.2;
-    let centners = kg / 100;
-    let tons = kg / 1000;
+        alert(`${kilogram} кілограм = ${pounds.toFixed(2)} фунтів`);
+        alert(`${kilogram} кілограм = ${ounces.toFixed(2)} унцій`);
+        alert(`${kilogram} кілограм = ${centners.toFixed(2)} центнерів`);
+        alert(`${kilogram} кілограм = ${tons.toFixed(2)} тонни`);
+        console.log(`${kilogram} кілограм = ${pounds.toFixed(2)} фунтів, ${ounces.toFixed(2)} унцій, ${centners.toFixed(2)} центнерів, ${tons.toFixed(2)} тонни `);
+    }
+    else{
+        alert('Помилка!');
 
-    alert(`${kg} кілограм = ${pounds.toFixed(2)} фунтів`);
-    alert(`${kg} кілограм = ${ounces.toFixed(2)} унцій`);
-    alert(`${kg} кілограм = ${centners.toFixed(2)} центнерів`);
-    alert(`${kg} кілограм = ${tons.toFixed(2)} тонни`);
+    }
 }
 
 
 function interest() {
-    let sum = prompt('Введіть суму депозиту:'); 
+    let sum = +prompt('Введіть суму депозиту:'); 
+    console.log(typeof(sum));
     let profit;
-    if(sum < 10000){
-        profit = sum / 100 * 15;
+
+    if(!isNaN(sum) && sum > 0){
+        if(sum < 10000){
+            profit = sum / 100 * 15;
+        }
+        else{
+            profit = sum / 100 * 17;
+        }
+        alert(`${profit} сума отримана за рік `);
+        console.log(`${profit} сума отримана за рік `);
     }
     else{
-        profit = sum / 100 * 17;
+        alert('Помилка!');
     }
-    alert(`${profit} сума отримана за рік `);
 
 }
