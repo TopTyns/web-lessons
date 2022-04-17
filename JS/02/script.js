d6f85b36087690875e8890c0fdd874c9ddecb396
TopTyns <dmitriubogutskiu2@gmail.com>
2022-04-17 14:06:48 +0300

    JS_02

---
+++ JS/02/script.js
@@ -0,0 +1,11 @@
+document.write('Задача 1 - просто текст<br>');
+document.write('<i>Задача 2 - курсивом</i><br>');
+
+let str = 'text';
+document.write('Задача 3 - ' + str.italics());
+document.write('<br>');
+
+for(let i = 1; i<=5; i++)
+{
+    document.write(i + '<br>');
+}
